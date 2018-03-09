c37cf20e8fb897a9ded1a94811ce96f6dd3b840a
JasonWu <wjasonq@gmail.com>
2018-03-08 22:54:35 -0800

    a

---
+++ 813. Find Anagram Mappings.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param A: lists A
+    @param B: lists B
+    @return: the index mapping
+    """
+    def anagramMappings(self, A, B):
+        lookup = collections.defaultdict(collections.deque)
+        for i, n in enumerate(B):
+            lookup[n].append(i)
+        result = []
+        for n in A:
+            result.append(lookup[n].popleft())
+        return result
